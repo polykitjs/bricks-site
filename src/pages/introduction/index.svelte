de090238ae4a3b441afeb1cf3edc390b1046d56a
jakobrosenberg <jakobrosenberg@gmail.com>
2019-11-21 18:28:21 +0100

    introduction

---
+++ src/pages/introduction/index.svelte
@@ -0,0 +1,17 @@
+<h1>Bricks</h1>
+<h3>Unstyled components for Svelte.</h3>
+
+<p>
+  There are plenty of CSS frameworks and prestyled components libraries. They're
+  great for prototypes, MVPs and projects that require a strict design
+  specification.
+</p>
+<p>
+  Often however, they're constraining and end up working against you, as your
+  app outgrows their scale and design.
+</p>
+<p>
+  Bricks aims to separate functionality from style. It does so by shipping with
+  only the style required for components to remain functional. The components
+  can then be extended and styled with CSS, and {'<slots>'}.
+</p>
